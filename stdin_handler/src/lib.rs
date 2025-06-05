9eb077c533b1cb0a07d14ccf3f535961d756cdf5
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-06-05 21:28:41 +0100

    Set cfg to termios to unix family only

--- stdin_handler/src/lib.rs
+++ stdin_handler/src/lib.rs
@@ -1,5 +1,5 @@
-use nix::sys::termios;
-use nix::sys::termios::{SetArg, Termios};
+#[cfg(target_family = "unix")]
+use nix::sys::termios::{SetArg, Termios, tcgetattr, cfmakeraw, tcsetattr};
 use std::io;
 use std::io::Read;
 
@@ -12,13 +12,13 @@ pub fn add(left: u64, right: u64) -> u64 {
 pub extern "C" fn init() -> i32{
     let stdin = io::stdin();
     let mut termios: Termios;
-    match termios::tcgetattr(stdin) {
+    match tcgetattr(stdin) {
         Result::Ok(T) => termios = T,
         Result::Err(_) => return 1,
     }
-    termios::cfmakeraw(&mut termios);
+    cfmakeraw(&mut termios);
     let stdin = io::stdin();
-    match termios::tcsetattr(stdin, SetArg::TCSANOW, &termios) {
+    match tcsetattr(stdin, SetArg::TCSANOW, &termios) {
         Result::Err(_) => return -2,
         _ => {}
     }
