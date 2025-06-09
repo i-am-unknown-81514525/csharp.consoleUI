b12d85ca0a39e8fd307082adbca5b6d90a4241c2
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-06-09 22:17:15 +0100

    Update to contain Global variable set

---
+++ Global.cs
@@ -0,0 +1,10 @@
+using ui.core;
+
+namespace ui
+{
+    public static class Global
+    {
+        // ReSharper disable once UnusedMember.Global
+        public static readonly RootInputHandler InputHandler = new RootInputHandler();
+    }
+}
