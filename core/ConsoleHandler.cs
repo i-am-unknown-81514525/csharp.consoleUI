68f823216769c9a120cd0beb6f96e1f6bdb1de5c
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-06-10 12:43:27 +0100

    fix ig

--- core/ConsoleHandler.cs
+++ core/ConsoleHandler.cs
@@ -11,13 +11,13 @@ namespace ui.core
     {
         private static class WindowConsoleHandler
         {
-            [DllImport("libstdin_handler", SetLastError = true, CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libstdin_handler.dll", SetLastError = true, CallingConvention = CallingConvention.Cdecl)]
             private static extern int init();
 
-            [DllImport("libstdin_handler", SetLastError = true, CallingConvention = CallingConvention.Cdecl)]
+            [DllImport("libstdin_handler.dll", SetLastError = true, CallingConvention = CallingConvention.Cdecl)]
             private static extern byte read_stdin();
 
-            [DllImport("libstdin_handler", SetLastError = true, CallingConvention =CallingConvention.Cdecl)]
+            [DllImport("libstdin_handler.dll", SetLastError = true, CallingConvention =CallingConvention.Cdecl)]
             private static extern int reset();
 
             public static byte readStdin() => read_stdin();
