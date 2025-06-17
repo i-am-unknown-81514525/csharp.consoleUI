13d9f85565bf5ac1bfb3624ce6e585bc9164fb93
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-06-17 22:13:55 +0100

    Include some keycode constant that is used

---
+++ const.cs
@@ -0,0 +1,17 @@
+namespace ui
+{
+    public enum KeyCode : byte
+    {
+        TAB = 9,
+        NEWLINE = 13,
+        SPACE = 32,
+        BACKSPACE = 127,
+        ARR_UP = 252, // SPECIAL: \x1b[A
+        ARR_DOWN = 253, // SPECIAL: \x1b[B
+        ARR_LEFT = 254, // SPECIAL: \x1b[C
+        ARR_RIGHT = 255, // SPECIAL: \x1b[D
+        SPECIAL_UNFOCUS = 250, // SPECIAL: \x1b[O
+        SPECIAL_FOCUS = 251, // SPECIAL: \x1b[I
+        DEL=249 // SPECIAL: \x1b[3~
+    }
+}
