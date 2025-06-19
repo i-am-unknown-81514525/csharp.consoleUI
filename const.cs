decf8192ab116b94f9d02af30a3750bde9e2cdcc
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-06-19 14:16:00 +0100

    Update KeyCode

--- const.cs
+++ const.cs
@@ -5,14 +5,20 @@ namespace ui
         INTERRUPT = 3,
         TAB = 9,
         NEWLINE = 13,
+        ESC = 27.
         SPACE = 32,
         BACKSPACE = 127,
+        INSERT = 245, // SPECIAL: \x1b[2~
+        PG_UP = 246, // SPECIAL: \x1b[5~
+        PG_DOWN = 247, // SPECIAL: \x1b[6~
+        HOME = 244, // SPECIAL: \x1b[H
+        END = 248, // SPECIAL: \x1b[F
+        DEL = 249, // SPECIAL: \x1b[3~
+        SPECIAL_UNFOCUS = 250, // SPECIAL: \x1b[O
+        SPECIAL_FOCUS = 251, // SPECIAL: \x1b[I
         ARR_UP = 252, // SPECIAL: \x1b[A
         ARR_DOWN = 253, // SPECIAL: \x1b[B
         ARR_LEFT = 254, // SPECIAL: \x1b[C
-        ARR_RIGHT = 255, // SPECIAL: \x1b[D
-        SPECIAL_UNFOCUS = 250, // SPECIAL: \x1b[O
-        SPECIAL_FOCUS = 251, // SPECIAL: \x1b[I
-        DEL=249 // SPECIAL: \x1b[3~
+        ARR_RIGHT = 255 // SPECIAL: \x1b[D
     }
 }
