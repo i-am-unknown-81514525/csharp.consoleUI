c4caa5f359ef202f4019f963f96c35f8b4ce3297
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-07-12 18:50:07 +0100

    Fix incorrect namespace for StrMagic.cs

--- core/KeyCodeTranslateHandler.cs
+++ core/KeyCodeTranslateHandler.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using ui.utils;
 
 namespace ui.core
 {
--- core/RootInputHandler.cs
+++ core/RootInputHandler.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 using static ui.core.ConsoleHandler.ConsoleIntermediateHandler;
+using ui.utils;
 
 namespace ui.core
 {
