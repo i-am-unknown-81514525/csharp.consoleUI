1bbbbc3690e5aa74b699d68a1618b1ea1ba2e950
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-08-06 15:57:10 +0100

    Cache ANSI Color constructor

--- fmt/TextFormatter.cs
+++ fmt/TextFormatter.cs
@@ -1,23 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 namespace ui.fmt
 {
     public static class TextFormatter
     {
+        private static Dictionary<ForegroundColor, string> foreCache = new Dictionary<ForegroundColor, string>();
+        private static Dictionary<BackgroundColor, string> backCache = new Dictionary<BackgroundColor, string>();
+        private static Dictionary<(ForegroundColor, BackgroundColor), string> forebackCache = new Dictionary<(ForegroundColor, BackgroundColor), string>();
+
         public static string Constructor(ForegroundColor foregroundColor)
         {
+            if (foreCache.ContainsKey(foregroundColor)) return foreCache[foregroundColor];
             string outStr = foregroundColor.ToString();
-            return $"\x1b[{outStr}m";
+            return foreCache[foregroundColor] = $"\x1b[{outStr}m";
         }
         public static string Constructor(BackgroundColor backgroundColor)
         {
+            if (backCache.ContainsKey(backgroundColor)) return backCache[backgroundColor];
             string outStr = backgroundColor.ToString();
-            return $"\x1b[{outStr}m";
+            return backCache[backgroundColor] = $"\x1b[{outStr}m";
         }
         public static string Constructor(ForegroundColor foregroundColor, BackgroundColor backgroundColor)
         {
+            if (forebackCache.ContainsKey((foregroundColor, backgroundColor))) return forebackCache[(foregroundColor, backgroundColor)];
             string outStr = $"{foregroundColor};{backgroundColor}";
-            return $"\x1b[{outStr}m";
+            return forebackCache[(foregroundColor, backgroundColor)] = $"\x1b[{outStr}m";
         }
 
         public static string Constructor(BackgroundColor backgroundColor, ForegroundColor foregroundColor)
