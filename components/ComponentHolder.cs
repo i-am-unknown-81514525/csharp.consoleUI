4a783485f9cd4996c208703e0378a3c429a701e1
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-09-14 15:26:33 +0100

    More updates

--- components/ComponentHolder.cs
+++ components/ComponentHolder.cs
@@ -1,7 +1,26 @@
+using System;
+
 namespace ui.components
 {
+    public class UninitComponentException : InvalidOperationException
+    {
+        public UninitComponentException(string err = null) : base(err ?? "Using component from ComponentHolder that is not initialized") { }
+    }
+
     public struct ComponentHolder<T> where T : IComponent
     {
-        public T comp { get; set; }
+        public T raw_inner { get; set; }
+        public T inner
+        {
+            get
+            {
+                if (!(raw_inner is object)) // is null don't work weirdly
+                {
+                    throw new UninitComponentException();
+                }
+                return raw_inner;
+            }
+            set => raw_inner = value;
+        }
     }
 }
--- components/PageSwitcher.cs
+++ components/PageSwitcher.cs
@@ -76,5 +76,76 @@ namespace ui.components
                 }
             };
         }
+
+
+        public PageSwitcher(ComponentHolder<Switcher> switcher, string content, int page) : base(content)
+        {
+            onClickHandler = (_, __) =>
+            {
+                if (required())
+                {
+                    switcher.inner.SwitchTo(page);
+                }
+            };
+        }
+
+        public PageSwitcher(ComponentHolder<Switcher> switcher, int page, string content = null) : base(content ?? $"Go To Page {page}")
+        {
+            onClickHandler = (_, __) =>
+            {
+                if (required())
+                {
+                    switcher.inner.SwitchTo(page);
+                }
+            };
+        }
+
+        public PageSwitcher(ComponentHolder<Switcher> switcher, Func<bool> required, string content, int page) : base(content)
+        {
+            this.required = required;
+            onClickHandler = (_, __) =>
+            {
+                if (this.required())
+                {
+                    switcher.inner.SwitchTo(page);
+                }
+            };
+        }
+
+        public PageSwitcher(ComponentHolder<Switcher> switcher, Func<bool> required, int page, string content = null) : base(content ?? $"Go To Page {page}")
+        {
+            this.required = required;
+            onClickHandler = (_, __) =>
+            {
+                if (this.required())
+                {
+                    switcher.inner.SwitchTo(page);
+                }
+            };
+        }
+
+        public PageSwitcher(ComponentHolder<Switcher> switcher, string content, Func<bool> required, int page) : base(content)
+        {
+            this.required = required;
+            onClickHandler = (_, __) =>
+            {
+                if (this.required())
+                {
+                    switcher.inner.SwitchTo(page);
+                }
+            };
+        }
+
+        public PageSwitcher(ComponentHolder<Switcher> switcher, int page, Func<bool> required, string content = null) : base(content ?? $"Go To Page {page}")
+        {
+            this.required = required;
+            onClickHandler = (_, __) =>
+            {
+                if (this.required())
+                {
+                    switcher.inner.SwitchTo(page);
+                }
+            };
+        }
     }
 }
